fb150f1dde0a6a8a8b6e78988773d9a76e0e1097
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2024-12-06 21:15:40 +0530

    Add copyright notice to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,6 +15,9 @@ const Footer = () => {
           </a>
         ))}
       </div>
+      <p className="mt-8 text-center tracking-tighter text-neutral-500">
+        &copy; Built by Shubhojit Mitra
+      </p>
     </div>
   );
 };
